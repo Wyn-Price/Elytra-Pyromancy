6f7adf0e2be1d8f9cac5e70c18d62cea5b5ec6ee
Wyn Price <wynprice999@gmail.com>
2018-04-21 00:17:27 +0100

    Fixed issues with changed modid

--- src/main/java/com/wynprice/fireworks/FireworksMod.java
+++ src/main/java/com/wynprice/fireworks/FireworksMod.java
@@ -18,8 +18,8 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 @Mod(modid = FireworksMod.MODID, name = FireworksMod.NAME, version = FireworksMod.VERSION)
 public class FireworksMod
 {
-    public static final String MODID = "elytrapyromancy";
-    public static final String NAME = "Elytra Pyromancy";
+    public static final String MODID = "fireworks";
+    public static final String NAME = "Fireworks";
     public static final String VERSION = "1.0";
     
     public static final CreativeTabs TAB = new CreativeTabs(MODID) {
