cfee0668537a023a259328791d704ddc884d2bd1
Wyn Price <wynprice999@gmail.com>
2018-04-22 21:43:49 +0100

    Fixed issue with new calculator package

--- src/main/java/com/wynprice/fireworks/common/util/MathReader.java
+++ src/main/java/com/wynprice/fireworks/common/util/MathReader.java
@@ -1,10 +1,13 @@
 package com.wynprice.fireworks.common.util;
 
+import com.wynprice.fireworks.common.util.calculator.MathCalculation;
+import com.wynprice.fireworks.common.util.calculator.MathReaderException;
+
 public class MathReader {//TODO: remove this class
 	public static double eval(String string) {
 		try {
 			return new MathCalculation(string).getOutput();
-		} catch (MathCalculation.MathReaderException e) {
+		} catch (MathReaderException e) {
 			throw new RuntimeException(e);
 		}
 	}
